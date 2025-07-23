75585bd2d3f0eb03f92bb400f78f534ad350c208
obakengshepherd <obakengtsaagane@gmail.com>
2025-07-23 22:57:36 +0200

    JS v1: Added best/avg time stats

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.getElementById('lapForm').addEventListener('submit', function(event) {
     lapData.push({ car: carName, time: lapTime });
     console.log("Lap added:", lapData);
     updateTable();
+    calculateStats();
 });
 
 function updateTable() {
@@ -15,4 +16,17 @@ function updateTable() {
         table += `<tr><td>${lap.car}</td><td>${lap.time.toFixed(2)}</td></tr>`;
     }
     document.getElementById('resultsTable').innerHTML = table;
+}
+
+function calculateStats() {
+    if (lapData.length === 0) {
+        document.getElementById('bestTime').innerText = 'N/A';
+        document.getElementById('avgTime').innerText = 'N/A';
+        return;
+    }
+    const times = lapData.map(lap => lap.time);
+    const bestTime = Math.min(...times);
+    const avgTime = times.reduce((sum, time) => sum + time, 0) / times.length;
+    document.getElementById('bestTime').innerText = bestTime.toFixed(2);
+    document.getElementById('avgTime').innerText = avgTime.toFixed(2);
 }
